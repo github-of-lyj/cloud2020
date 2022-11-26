a5ade8593e6c1163b3e1099ceb0b88638e703833
Alex <Alex@lyj.com>
2022-11-26 18:36:07 +0800

    2022.11.26

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/Service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/Service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.Service;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface PaymentService {
 
     public String paymentInfo_OK(Integer ID);
@@ -8,6 +10,8 @@ public interface PaymentService {
     public String paymentInfo_TimeOut(Integer ID);
 
     public String paymentInfo_TimeOutHandler(Integer id);
+
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id);
 
 
 }
